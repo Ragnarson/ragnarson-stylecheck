2045b7ad4aab7bb5ba78b4c3862feaa56a0d0124
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2016-03-10 14:41:02 +0100

    Allow to use local rubocop config by rake task.
    In some projects rubocop returns:
    ```
    (Using Ruby 2.0 parser; configure using TargetRubyVersion parameter, under AllCops)
    ```
    
    Different projects using different ruby versions. To fix this problem user should set TargetRubyVersion inside rubocop config.
    
    ```
    inherit_gem:
      ragnarson-stylecheck: config/rubocop.yml
    
    AllCops:
      TargetRubyVersion: 2.3
    ```

--- lib/ragnarson/stylecheck/rubocop_helpers.rb
+++ lib/ragnarson/stylecheck/rubocop_helpers.rb
@@ -6,6 +6,22 @@ module Ragnarson
     module RubocopHelpers
       class << self
         def config
+          if project_config_exists?
+            project_config_path
+          else
+            stylecheck_config_path
+          end
+        end
+
+        def project_config_exists?
+          File.exist?(project_config_path)
+        end
+
+        def project_config_path
+          File.join(Dir.pwd, "config", "rubocop.yml")
+        end
+
+        def stylecheck_config_path
           File.join(Ragnarson::Stylecheck.root, "config", "rubocop.yml")
         end
       end
